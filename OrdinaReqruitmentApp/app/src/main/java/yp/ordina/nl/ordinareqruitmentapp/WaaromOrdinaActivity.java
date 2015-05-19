eebdeb99269d1707a44943e1a7911a43f147ba55
Sinan Ozdil <sinan.ozdil@ordina.nl>
2015-05-19 14:26:36 +0200

    imagebutton replaced by button waarom Ordina activity

--- OrdinaReqruitmentApp/app/src/main/java/yp/ordina/nl/ordinareqruitmentapp/WaaromOrdinaActivity.java
+++ OrdinaReqruitmentApp/app/src/main/java/yp/ordina/nl/ordinareqruitmentapp/WaaromOrdinaActivity.java
@@ -1,27 +1,24 @@
 package yp.ordina.nl.ordinareqruitmentapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class WaaromOrdinaActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_waarom_ordina);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_waarom_ordina, menu);
         return true;
     }
 
@@ -38,10 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startWaaromOrdinaActivity(View view){
-        Intent intent = new Intent(this, WaaromOrdinaActivity.class);
-        startActivity(intent);
     }
 }
