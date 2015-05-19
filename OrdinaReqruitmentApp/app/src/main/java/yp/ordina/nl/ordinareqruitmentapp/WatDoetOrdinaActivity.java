3d2ce2fe63e04fc52b19d9b305b894e72382eef6
AhmedOrd <at@Ahmeds-MacBook-Pro.local>
2015-05-19 15:27:48 +0200

    Wat doet Ordina Activity is klaar
    Activity is klaar

--- OrdinaReqruitmentApp/app/src/main/java/yp/ordina/nl/ordinareqruitmentapp/WatDoetOrdinaActivity.java
+++ OrdinaReqruitmentApp/app/src/main/java/yp/ordina/nl/ordinareqruitmentapp/WatDoetOrdinaActivity.java
@@ -1,27 +1,23 @@
 package yp.ordina.nl.ordinareqruitmentapp;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class WatDoetOrdinaActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wat_doet_ordina);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_wat_doet_ordina, menu);
         return true;
     }
 
@@ -39,14 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void startWaaromOrdinaActivity(View view){
-        Intent intent = new Intent(this, WaaromOrdinaActivity.class);
-        startActivity(intent);
-    }
-    public void startWatDoetOrdinaActivity(View view){
-        Intent intent = new Intent(this, WatDoetOrdinaActivity.class);
-        startActivity(intent);
-    }
 }
-
