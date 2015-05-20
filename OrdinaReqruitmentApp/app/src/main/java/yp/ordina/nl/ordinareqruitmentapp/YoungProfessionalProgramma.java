5707ded1674108abac4ff2ae388093bfe2e054de
reneberghuis <reneberghuis@hotmail.com>
2015-05-20 10:22:41 +0200

    Alle activiteiten toegevoegd, Settings button uitgeschakeld, menubalk disabled in mainactivity

---
+++ OrdinaReqruitmentApp/app/src/main/java/yp/ordina/nl/ordinareqruitmentapp/YoungProfessionalProgramma.java
@@ -0,0 +1,45 @@
+package yp.ordina.nl.ordinareqruitmentapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import yp.ordina.nl.ordinareqruitmentapp.R;
+import yp.ordina.nl.ordinareqruitmentapp.WaaromOrdinaActivity;
+import yp.ordina.nl.ordinareqruitmentapp.WatDoetOrdinaActivity;
+
+public class YoungProfessionalProgramma extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_young_professional_programma);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_young_professional_programma, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
